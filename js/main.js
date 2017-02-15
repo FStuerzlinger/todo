e86f01fc77cf1df8c213058928a5a9c14f0a6bbd
Florian Stürzlinger <stueflo@gmail.com>
2017-02-15 22:31:37 +0100

    added a delete button and change the structure of the notes

--- js/main.js
+++ js/main.js
@@ -10,21 +10,17 @@ document.getElementById('name-input-form').onsubmit = function(e) {
   localStorage.setItem("name", document.getElementById('input-name').value);
   // Show the new elements
   setTimeout(() => {
-
     // Switch the display style
     document.getElementById('header-text').innerHTML = localStorage.name +
       "'s notes";
-    document.getElementById('main-page').style.display = 'initial';
-
     document.getElementById('welcome-page').style.display = 'none';
-
     // Animations not working yet
-    //document.getElementById('header-after').className = "r";
-    //document.getElementById('content-after').className = "r";
-    //document.getElementById('footer-after').className = "r";
+    document.getElementById('main-page').className = "r";
+    document.getElementById('main-page').style.display = 'initial';
   }, 400);
 };
 
+// Creates a new Note on the main page
 document.getElementById('note-input-form').onsubmit = function(e) {
   // Cancle the form submit
   e.preventDefault();
@@ -33,11 +29,31 @@ document.getElementById('note-input-form').onsubmit = function(e) {
   var notes = [];
   notes.push(document.getElementById('input-note').value);
   for (var i = 0; i < notes.length; i++) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(notes[i]);
-    node.appendChild(textnode);
-    document.getElementById("notes-list").appendChild(node);
+
+    // Create the row
+    var row = document.createElement("div");
+    row.className = "row";
+    // div for the text content of the note
+    var node = document.createElement("div");
+    node.className = "note-element";
+    node.contentEditable = true;
+    node.innerHTML = notes[i];
+    // Element for the button
+    var control = document.createElement("div");
+    control.className = "control-block";
+    // add the button to the control block
+    var button = document.createElement("button");
+    var text = document.createTextNode("X");
+    button.appendChild(text);
+    control.appendChild(button);
+
+    // add the divs to the row
+    row.appendChild(node);
+    row.appendChild(control);
+
+    //node.appendChild(textnode);
+    document.getElementById("notes").appendChild(row);
   }
   document.getElementById('input-note').value = "";
-
+  // document.getElementById('editable').textContent.trim()
 };
